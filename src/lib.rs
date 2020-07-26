2db01a30e4973a0e0e4200a1303ebe9bff9868f9
Sam Szuflita <sam.szuflita@gmail.com>
2020-07-26 15:52:43 -0400

    to_full_word works

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,39 @@
+use std::collections::HashSet;
+
 fn from_full_word(full_word: &String) -> Result<String, ()> {
     if full_word.len() < 3 {
-        return Err(())
+        return Err(());
     }
     match full_word.chars().next() {
         Some(first) => match full_word.chars().last() {
             Some(last) => {
-
                 let n = full_word.len() - 2;
-                return Ok(std::format!("{}{}{}", first, n, last))
+                return Ok(std::format!("{}{}{}", first, n, last));
             }
             None => return Err(()),
         },
         None => return Err(()),
     }
+}
+
+fn to_full_word<'a>(numeronym: &String, words: HashSet<String>) -> Result<HashSet<String>, ()> {
+    // parse out a, n, b
+    if numeronym.len() < 3 {
+        return Err(());
+    }
+
+    let mut results = HashSet::new();
+
+    words.iter().for_each(|word| match from_full_word(word) {
+        Ok(candidate_numeronym) => {
+            if candidate_numeronym == *numeronym {
+                results.insert(word.clone());
+            }
+        }
+        Err(_) => {}
+    });
+
+    Ok(results)
 }
 
 #[cfg(test)]
@@ -48,5 +69,18 @@ mod tests {
         let actual = from_full_word(&empty);
         let expected: Result<String, ()> = Err(());
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn to_full_word_works() {
+        let mut words: HashSet<String> = HashSet::new();
+        words.insert(String::from("kubernetes"));
+        words.insert(String::from("asdf"));
+
+        let actual = to_full_word(&String::from("k8s"), words); 
+
+        let mut expected = HashSet::new();
+        expected.insert(String::from("kubernetes"));
+        assert_eq!(actual.unwrap(), expected);
     }
 }
